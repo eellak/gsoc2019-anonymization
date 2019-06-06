4877b995623297363c3048e74c9fe62445c4ae0f
Dimitris Katsiros <dkatsiros@gmail.com>
2019-06-06 19:36:11 +0300

    Add name search, 2152 greek names

--- anonymizer/trie_index.py
+++ anonymizer/trie_index.py
@@ -8,15 +8,22 @@ def read_dictionary(dictionary_file=None):
     dictionary_file = 'male_and_female_names.txt' if dictionary_file == None else dictionary_file
     with open(dictionary_file, 'r') as f:
         file = f.readlines()
-        data = [word.strip().replace(' ', '') for word in file]
+        data = [prepair_word(word.strip().replace(' ', '').lower()).upper()
+                for word in file]
         return data
 
 
 def prepair_word(word=''):
 
+    # Lowercase word
+    word = word.lower()
+
     # Remove tonos
-    without_tonos = ['α', 'ε', 'η', 'ι', 'ο', 'υ', 'ω', 'ι']
-    with_tonos = ['ά', 'έ', 'ή', 'ί', 'ό', 'ύ', 'ώ', 'ϊ']
+    without_tonos = ['α', 'ε', 'η', 'η', 'ι', 'ι', 'ι', 'ι',
+                     'ο', 'υ', 'υ', 'υ', 'υ', 'ω',  'ω']
+
+    with_tonos = ['ά', 'έ', 'ή', 'ἡ', 'ί', 'ἰ', 'ϊ', 'ΐ',
+                  'ό', 'ύ', 'ϋ', 'ῦ', 'ῦ', 'ώ', 'ῶ']
 
     for letter_index, letter in enumerate(with_tonos):
         try:
@@ -27,14 +34,24 @@ def prepair_word(word=''):
             word = word.replace(
                 word[word_index], without_tonos[letter_index])
 
-    return word.lower()
+    return word
+
+
+def read_dictionary(dictionary_file=None):
+
+    dictionary_file = 'male_and_female_names.txt' if dictionary_file == None else dictionary_file
+    with open(dictionary_file, 'r') as f:
+        file = f.readlines()
+        data = [prepair_word(word.strip().replace(' ', ''))
+                for word in file]
+        return data
 
 
 class TrieNode:
 
     # Trie node class
     def __init__(self):
-        self.children = [None]*26
+        self.children = [None]*25
 
         # isEndOfWord is True if node represent the end of the word
         self.isEndOfWord = False
@@ -97,13 +114,21 @@ def identify(dataset=None, testwords=[]):
     dataset = 'anonymizer/data/male_and_female_names.txt' if dataset == None else dataset
     data = read_dictionary(dictionary_file=dataset)
     # Input words (prepaired to only 'α' through 'ω' and lower case)
-    dictionary = [prepair_word(word) for word in data]
+    dictionary = [word for word in data]
     # Trie object
     t = Trie()
 
     # Construct trie
     for word in dictionary:
-        t.insert(word)
+        try:
+            t.insert(word)
+        except:
+            from termcolor import colored as color
+            print(color(
+                  'The following name cannot be included in the search:{}'.format(
+                      word),
+                  'red'))
+
     # Possible names. Words that start with uppercase letter
     possible_names = []
     name_found = []
