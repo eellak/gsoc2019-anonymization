20797cdd01f2c9abceb479d752320eeca7632a13
Dimitris Katsiros <dkatsiros@gmail.com>
2019-07-04 21:35:58 +0300

    Add about and contact page

--- src/anonymizer/urls.py
+++ src/anonymizer/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view
+from pages.views import home_view, about_view, contact_view
 
 urlpatterns = [
+    path('about/', about_view, name='about'),
     path('home/', home_view, name='home'),
+    path('contact/', contact_view, name='home'),
     path('admin/', admin.site.urls),
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    return render(request, 'about.html', {})
+
+
+def contact_view(request, *args, **kwargs):
+    return render(request, 'contact.html', {})
