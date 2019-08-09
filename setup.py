9e3395ae46e178eb90e6cc95e8f52ff0a4140277
Dimitris Katsiros <dkatsiros@gmail.com>
2019-08-09 18:04:45 +0300

    Fix setup.py including data files

--- setup.py
+++ setup.py
@@ -5,17 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='anonymizer',
-    version="0.0.4",
-    # scripts=['__init__.py', '__main__.py'],
+    version="0.0.5",
     author="Dimitris Katsiros",
     author_email="dkatsiros@gmail.com",
     description="Anonymizer Service implemented for GSoC 2019",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/eellak/gsoc2019-anonymization",
-    # packages=setuptools.find_packages(),
     packages=['anonymizer'],
-    include_package_data=True,
+    package_data={'anonymizer': ['data/*']},
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
