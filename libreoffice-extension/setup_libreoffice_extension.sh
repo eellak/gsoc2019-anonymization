c403b352651892be4c0db5d72242eca8f140a9e7
Dimitris Katsiros <dkatsiros@gmail.com>
2019-08-26 20:43:07 +0300

    Update setup script

--- libreoffice-extension/setup_libreoffice_extension.sh
+++ libreoffice-extension/setup_libreoffice_extension.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 DIR="$(find / -name 'gsoc2019-anonymization' -printf '%h\n' -quit)/gsoc2019-anonymization/libreoffice-extension/";
-cp $DIR/anonymizer_extension.py /usr/lib/libreoffice/share/Scripts/python/;
+SEARCH="/home/$USER/snap/libreoffice/144/.config/libreoffice/4/user"
+DEST="$SEARCH/Scripts/python"
+mkdir -p $DEST
+cp $DIR/anonymizer_extension.py $DEST/ 
+#cp $DIR/anonymizer_extension.py /usr/lib/libreoffice/share/Scripts/python/;
 echo 'Anonymizer extension was installed successfully ..'
